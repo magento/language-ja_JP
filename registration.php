49e20fcc827e1a67cfb3d212506eee86f1edaeb3
magentoj <magentoj@adobe.com>
2021-03-11 17:43:46 +0900

    first commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © Adobe, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'magentoj_ja_jp', __DIR__);
+
